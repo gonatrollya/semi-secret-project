b845c1f02e926f29a040d5fb52bea5e97340978b
gonatrollya <41080125+gonatrollya@users.noreply.github.com>
2019-08-14 19:05:02 -0400

    i rlly shouldn be alowed to design things
    yikes, this is beyond horendus like what in the actual, why would i do this

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 import sqlite3
+from tkinter import *
+
+def login_screen():
+    global screen
+    screen = Tk()
+    screen.geometry("400x300")
+    screen.title("Start")
+    screen.configure(background='pink')
+    Label(text = "Select",bg = "orange",fg = 'purple' ,width = "300", height = "2",font = ("Calibri", 13)).pack()
+    Label(text = "",bg = 'green').pack()
+    Button(text = "Spin Dice", height = "2" , width = "30").pack()
+    Label(text = "",bg = 'yellow').pack()
+    Button(text = "Create Character", height = "2" , width = "30").pack()
+    Label(text = "",bg = 'darkgrey').pack()
+    Button(text = "Select Campaign", height = "2" , width = "30").pack()
+    Label(text = "",bg = 'purple').pack()
+    Button(text = "Continue", height = "2" , width = "30" ).pack()
+    screen.mainloop()
+
+
+login_screen()
